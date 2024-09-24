e33cde312d1616afd513418905e534ef23b9e007
Fahrizp24 <Fpradian24@gmail.com>
2024-09-24 21:37:57 +0700

    Soal no 1

---
+++ Pertemuan5/PHP02/fungsi.php
@@ -0,0 +1,11 @@
+<?php 
+
+function perkenalan(){
+    echo"Assalamulaikum, ";
+    echo"Perkenalkan, nama saya Fahri <br/>";
+    echo "Senang berkenalan dengan Anda <br/>";
+}
+
+perkenalan();
+perkenalan();
+?>
