ddfc1ca7dcdde66d94f91d0b006561450e431c9e
Fahrizp24 <Fpradian24@gmail.com>
2024-10-01 12:28:35 +0700

    no 5

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -1,10 +1,15 @@
-<?php
-function perkenalan(){
-    echo"Assalamualaikum, ";
-    echo"Perkenalkan, saya Fahri <br>";
-    echo "Senang berkenalan dengan anda <br>";
+
+<?php 
+function perkenalan($nama, $salam){
+    echo"$salam, ";
+    echo"Perkenalkan, nama saya $nama <br/>";
+    echo "Senang berkenalan dengan Anda <br/>";
 }
 
-perkenalan();
-perkenalan();
+perkenalan("Hamdana","Hello");
+echo "<hr>";
+$saya = "Fahri";
+$ucapanSalam = "Selamat Pagi";
+
+perkenalan($saya,$ucapanSalam);
 ?>
