08bdb151af51f834d2ca051849afc7de41d93431
Fahrizp24 <Fpradian24@gmail.com>
2024-10-01 12:29:24 +0700

    Soal no 6

--- Pertemuan6/fungsi.php
+++ Pertemuan6/fungsi.php
@@ -1,15 +1,15 @@
 
 <?php 
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo"$salam, ";
     echo"Perkenalkan, nama saya $nama <br/>";
     echo "Senang berkenalan dengan Anda <br/>";
 }
 
 perkenalan("Hamdana","Hello");
-echo "<hr>";
+echo "<hr/>";
 $saya = "Fahri";
 $ucapanSalam = "Selamat Pagi";
 
-perkenalan($saya,$ucapanSalam);
+perkenalan($saya);
 ?>
