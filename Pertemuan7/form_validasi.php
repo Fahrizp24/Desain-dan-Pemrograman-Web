86ee81e893af0450e7270496992504a76730a366
Fahrizp24 <Fpradian24@gmail.com>
2024-10-13 15:54:07 +0700

    soal 7.3

--- Pertemuan7/form_validasi.php
+++ Pertemuan7/form_validasi.php
@@ -20,30 +20,54 @@
             <input type="submit" value="submit">
         </form>
 
+        <div id="hasil"></div>
+
         <script>
+            // $(document).ready(function () {
+            //     $("#myForm").submit(function (event) {
+            //         var email = $("#email").val();
+            //         var nama = $("#nama").val();
+            //         var valid = true;
+
+            //         if (nama === "") {
+            //             $("#nama-error").text("Nama harus diisi");
+            //             valid = false;
+            //         }else{
+            //             $("#nama-error").text("");
+            //         }
+
+            //         if (email==="") {
+            //             $("#email-error").text("email harus diisi.");
+            //             valid = false;
+            //         }else{
+            //             $("#email-error").text("");
+            //         }
+
+            //         if (!valid) {
+            //             (!event.preventDefault());
+            //         }
+            //     })
+            // })
+
             $(document).ready(function () {
-                $("#myForm").submit(function (event) {
+                $("#myForm").submit(function (e) {
+                    e.preventDefault();
                     var email = $("#email").val();
                     var nama = $("#nama").val();
                     var valid = true;
 
-                    if (nama === "") {
-                        $("#nama-error").text("Nama harus diisi");
-                        valid = false;
-                    }else{
-                        $("#nama-error").text("");
-                    }
+                    //mengumpulkan data form
+                    var formData = $("#myForm").serialize();
 
-                    if (email==="") {
-                        $("#email-error").text("email harus diisi.");
-                        valid = false;
-                    }else{
-                        $("#email-error").text("");
-                    }
-
-                    if (!valid) {
-                        (!event.preventDefault());
-                    }
+                    //kirim data ke server php
+                    $.ajax({
+                        url:"proses_validasi.php",
+                        type:"POST",
+                        data: formData,
+                        success: function (response){
+                            $("#hasil").html(response);
+                        }
+                    })
                 })
             })
         </script>
