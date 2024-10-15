ad57bc69f0927a04d768913fc1e2e94731dbad8f
Fahrizp24 <Fpradian24@gmail.com>
2024-10-15 16:08:13 +0700

    tugas 6 7

--- Pertemuan8/uploud.php
+++ Pertemuan8/uploud.php
@@ -1,17 +1,17 @@
 <?php
 if (isset($_POST["submit"])) {
     $targetdir="uploads/";
-    $targetfile=$targetdir.basename($_FILES["myFile"]["name"]);
+    $targetfile=$targetdir . basename($_FILES["myFile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile,PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg","jpeg","png","gif");
-    $maxsize = 5*1024*1024;
+    $allowedExtensions = array("txt","pdf","doc","docx");
+    $maxsize = 1.8*1024*1024;
     
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myFile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myFile"]["tmp_name"], $targetfile)){
             echo"File berhasil diunggah. <br>";
-            echo "<img src='".$targetfile."' style='max-width:200px;'><br>";
+            // echo "<img src='".$targetfile."' style='max-width:200px;'><br>";
         }else {
             echo"Gagal mengunggah file";
         }
