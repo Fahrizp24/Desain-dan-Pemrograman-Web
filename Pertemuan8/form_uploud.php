4f8f1b1e2572ace9c8e3b40e436749a1d510e3d1
Fahrizp24 <Fpradian24@gmail.com>
2024-10-15 14:50:43 +0700

    Pertemuan 8 pertanyaan 1

---
+++ Pertemuan8/form_uploud.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>File uploud</title>
+    </head>
+    <body>
+        <form action="uploud.php" method="POST" enctype="multipart/form-data">
+            <input type="file" name="myFile">
+            <input type="submit" name="submit">
+        </form>
+    </body>
+</html>
